0f16bad2ceee2ef93f8c9f128b33ed3e2741b335
PatskevichS <LuRonBel@gmail.com>
2020-02-18 12:12:13 +0300

    Initial commit

---
+++ src/main/java/gmail/luronbel/snakefx/Application.java
@@ -0,0 +1,8 @@
+package gmail.luronbel.snakefx;
+
+public class Application {
+
+    public static void main(String[] args){
+        System.out.println("Hello world! There will be something great!");
+    }
+}
---
+++ src/test/java/gmail/luronbel/snakefx/ApplicationTest.java
@@ -0,0 +1,10 @@
+package gmail.luronbel.snakefx;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
